b1e0a6eda70c04cc8ae46b10e83e75ee628fab6e
linazhang12 <linazhang13579@gmail.com>
2018-07-12 19:33:44 -0500

    updates for responsive site

--- main.js
+++ main.js
@@ -1,78 +1,36 @@
-var sound = "nothing";
-var timer = "nothing";
-
 function set() {
 	var timer = parseInt(document.querySelector("#timer").value);
 	var sound = document.querySelector("#sound").value;
 	document.getElementById("disable").disabled = true; 
 	var countdownSec = timer * 60;
 
+	var x = setInterval(function() {
 
-
-	if(sound == "water"){
-		soundfire.pause();
-		soundbird.pause();
-		soundwater.loop = true;
-		soundwater.play();
-			var x = setInterval(function() {
-				var min = Math.floor(countdownSec / 60);
-				var sec = countdownSec % 60;			
-				var time = min + ":" +sec;
-				document.getElementById("logo").innerHTML = time;
-				countdownSec --;
-					if (countdownSec < 0){
-						document.getElementById("logo").innerHTML = "meditate";
-						soundwater.pause();
-						document.getElementById("disable").disabled = false; 
-						clearInterval(x);
-					}
-			}, 1000);
-
-	} 
-	else if (sound == "fire"){
-		soundwater.pause();
-		soundbird.pause();
-		soundfire.loop = true;
-		soundfire.play();
-			var x = setInterval(function() {
-				var min = Math.floor(countdownSec / 60);
-				var sec = countdownSec % 60;			
-				var time = min + ":" +sec;
-				document.getElementById("logo").innerHTML = time;
-				countdownSec --;
-					if (countdownSec <= 0){
-						document.getElementById("logo").innerHTML = "meditate";
-						soundfire.pause();
-						document.getElementById("disable").disabled = false; 
-						clearInterval(x);
-					}
-			}, 1000);
-	} 
-	else{
-		soundfire.pause();
-		soundwater.pause();
-		soundbird.loop = true;
-		soundbird.play();
-			var x = setInterval(function() {
-				var min = Math.floor(countdownSec / 60);
-				var sec = countdownSec % 60;			
-				var time = min + ":" +sec;
-				document.getElementById("logo").innerHTML = time;
-				countdownSec --;
-					if (countdownSec <= 0){
-						document.getElementById("logo").innerHTML = "meditate";
-						soundbird.pause();
-						document.getElementById("disable").disabled = false; 
-						clearInterval(x);
-					}
-			}, 1000);
-	}
-
-	return;
+		if(sound == "water"){
+			soundwater.loop = true;
+			soundwater.play();
+		} 
+		else if (sound == "fire"){
+			soundfire.loop = true;
+			soundfire.play();
+		} 
+		else{
+			soundbird.loop = true;
+			soundbird.play();
+		}
+			var min = Math.floor(countdownSec / 60);
+			var sec = countdownSec % 60;			
+			var time = min + ":" + sec;
+			document.getElementById("logo").innerHTML = time;
+			countdownSec --;
+				if (countdownSec < 0){
+					document.getElementById("logo").innerHTML = "meditate";
+					soundwater.pause();
+					soundfire.pause();
+					soundbird.pause();
+					document.getElementById("disable").disabled = false; 
+					clearInterval(x);
+				}
+	}, 1000);
+return;
 }
-
-
-		
-
-
-
